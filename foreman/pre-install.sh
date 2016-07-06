33714a41fd3ca0d3deaa4725b7d8ac10abbd7fcb
Ilya Letkowski <ilya.letkowski@synesis.ru>
2016-07-06 15:58:02 +0300

    Init script

---
+++ foreman/pre-install.sh
@@ -0,0 +1,21 @@
+# Info
+# https://theforeman.org/manuals/1.12/index.html#2.Quickstart
+
+apt-get -y install ca-certificates
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+dpkg -i puppetlabs-release-pc1-xenial.deb
+
+echo "deb http://deb.theforeman.org/ xenial 1.12" > /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 1.12" >> /etc/apt/sources.list.d/foreman.list
+apt-get -y install ca-certificates
+wget -q https://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+
+apt-get update && apt-get -y install foreman-installer
+
+# Check
+# Ensure that ping $(hostname -f) shows the real IP address, not 127.0.1.1. Change or remove this entry from /etc/hosts if present.
+# /etc/init.d/networking restart
+
+# Run
+# foreman-installer
+
